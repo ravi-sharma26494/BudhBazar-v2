c01c4971eff3e36481fe9b555e02528668e4b438
Ravi Sharma <ravi.sharma26494@gmail.com>
2024-01-16 19:21:12 +0530

    first commit

---
+++ src/store/categories/category.reducer.ts
@@ -0,0 +1,54 @@
+import { AnyAction } from 'redux';
+
+import { CATEGORIES_ACTION_TYPES, Category } from './category.types';
+import {CategoryAction, fetchCategoriesFailed, fetchCategoriesStart, fetchCategoriesSuccess} from './category.action';
+
+export type CategoriesState = {
+  readonly  categories: Category[];
+  readonly  isLoading: boolean;
+  readonly  error: Error | null;
+};
+
+export const CATEGORIES_INITIAL_STATE: CategoriesState = {
+  categories: [],
+  isLoading: false,
+  error: null,
+};
+
+export const categoriesReducer = (
+  state = CATEGORIES_INITIAL_STATE,
+  action = {} as AnyAction
+):CategoriesState => {
+  
+  if(fetchCategoriesStart.match(action)){
+    return { ...state, isLoading: true };
+  }
+
+  if(fetchCategoriesSuccess.match(action)){
+    return { ...state, categories: action.payload, isLoading: false };
+  }
+
+  if(fetchCategoriesFailed.match(action)){
+    return { ...state, error: action.payload, isLoading: false };
+  }
+
+  return state;
+};
+
+// export const categoriesReducerOld = (
+//   state = CATEGORIES_INITIAL_STATE,
+//   action = {} as CategoryAction // a discriminatory union - can cause problems, when the default state is not given, so to improve it we are using with matcher utility
+// ):CategoriesState => {
+//   // const { type, payload } = action;
+
+//   switch (action.type) {
+//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+//       return { ...state, isLoading: true };
+//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+//       return { ...state, categories: action.payload, isLoading: false };
+//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+//       return { ...state, error: action.payload, isLoading: false };
+//     default:
+//       return state;
+//   }
+// };
